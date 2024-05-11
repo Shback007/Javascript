ef4f12f1a171f82100e5903697f58d7356dc3beb
Sagnik Hore <120464063+Shback007@users.noreply.github.com>
2024-05-11 13:24:09 +0000

    update datatypes-summary

--- 01_Basics/datatypes-summary.js
+++ 01_Basics/datatypes-summary.js
@@ -32,6 +32,32 @@ const MyFunction =function(){
     console.log("Hello World")
 }
 
-console.log(typeof(bigNumber))
+// console.log(typeof(bigNumber))
 
-console.log(typeof(heros))
+// console.log(typeof(heros))
+
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++
+
+//stack(Primitive), Heap(Non-Primitive)
+
+let myYoutubename="sagnikrock"
+
+let anothername=myYoutubename
+anothername="chaiaurcode"
+
+// console.log(myYoutubename);
+// console.log(anothername);
+
+let user1={
+    email:"user1@gmail.com",
+    upi:"user1@ybl"
+}
+
+let user2=user1
+
+user2.email="sagnik@gmail.com"
+
+console.log(user1.email)
+console.log(user2.email)
